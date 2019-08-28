c5b33c94ac10392624915e10bf57e8afae87ba11
jorge <j.m.paravicini@gmail.com>
2019-08-28 21:19:32 +0200

    Updated remaining files

--- TournamentMaster.py
+++ TournamentMaster.py
@@ -1,9 +1,36 @@
 from bots import BaseBot
+from bots.botutilities import Grid
 
 
 class TournamentMaster:
-    def __init__(self, bot_1: BaseBot, bot_2: BaseBot):
+
+    def __init__(self, bot_1: BaseBot, bot_2: BaseBot, grid_width: int, grid_height: int):
         self.bot_1 = bot_1
         self.bot_2 = bot_2
+        self.turn = 0
+        self.grid = Grid.create(grid_width, grid_height)
 
+    def current_turns_bot(self):
+        """
+        Selects which bot's turn it is. Can be overriden for different starting techniques or multiple turns in a row.
+        :return: The bot which gets the next turn.
+        """
+        if self.turn % 2 == 0:
+            return self.bot_1
+        else:
+            return self.bot_2
 
+    def play_turn(self):
+        bot = self.current_turns_bot()
+        bot.update_grid(self.grid)
+        decision = bot.get_guess()
+        if self.validate_guess(decision):
+            self.set_chip_at(decision, bot.id)
+
+        self.turn += 1
+
+    def validate_guess(self, guess: int) -> bool:
+        return True
+
+    def set_chip_at(self, column: int, bot_id: int):
+        pass
--- bots/botutilities/grid.py
+++ bots/botutilities/grid.py
@@ -1,8 +1,41 @@
-from typing import TypeVar
+from __future__ import annotations
+from typing import TypeVar, List, Generic, Callable
+import warnings
 
-# Type variable
 T = TypeVar('T')
 
-class Grid:
-    def __init__(self):
-        pass
+
+class Grid(Generic[T]):
+    def __init__(self, grid: List[List[T]]):
+        self.__grid = grid
+        if len(grid) > 0 and len(grid[0]) > 0:
+            self.__width = len(grid[0])
+            self.__height = len(grid)
+        else:
+            warnings.warn("Grid has invalid size.")
+            
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @classmethod
+    def create(cls, width: int, height: int, default: T = 0) -> Grid[T]:
+        return cls([x[:] for x in [[default] * width] * height])
+
+    def at(self, x: int, y: int) -> T:
+        return self.__grid[y][x]
+
+    # Declare generic parameter for Mapped Callback
+    S = TypeVar('S')
+
+    # We map all values from the current Grid which all have type T, to type S
+    def map(self, func: Callable[[T], S]) -> Grid[S]:
+        return Grid([list(map(func, row)) for row in self.__grid])
+
+    def print(self):
+        for row in self.__grid:
+            print(row)
