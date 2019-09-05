2da86c7943cac55e3f3ce91d6ab06a7329ece13b
jmparavicini <j.m.paravicini@gmail.com>
2019-09-05 11:26:23 +0200

    Updated Formatting of Command Tournament Master output

--- tournamentmasters/command_tournament_master.py
+++ tournamentmasters/command_tournament_master.py
@@ -6,6 +6,7 @@ class CommandTournamentMaster(TournamentMaster):
 
     def on_turn_end(self, bot_played: BaseBot):
         self.grid.print()
+        print("---------------------\n")
 
     def on_winner_found(self, winner_bot: BaseBot):
-        print(f'Bot {winner_bot.id} WOOOOOOON')
+        print(f'{winner_bot.name} {winner_bot.id} WOOOOOOON')
